76859aa5f984e88d00869681233f00234eac4d85
Jarred Tsibidis-Goldberg <jarredtsibidis-goldberg@Jarreds-MacBook-Pro.local>
2025-01-24 15:51:22 -0500

    hi

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "You are Stinky\n";
 
     for(int i = 1; i > 0; i++){
-        cout << "Get forever looped loser\n"
+        cout << "Get forever looped loser\n";
     }
 
     return 0;
