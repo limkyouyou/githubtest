b010a90b4ea7ca533a372abf9eb626864e1c66ac
Jarred Tsibidis-Goldberg <jarredtsibidis-goldberg@Jarreds-MacBook-Pro.local>
2025-01-24 17:28:17 -0500

    End of session.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,8 @@
 using namespace std;
 int main() {
 
-    std::cout << "We da best\n";
+    std::cout << "We da worst!\n";
 
-<<<<<<< HEAD
-=======
-    // Worst algorithm ever
-    for(int i = 1; i > 0; i++){
-        cout << "Get forever looped loser\n";
-    }
->>>>>>> 340770509d70609e1265276dd950bc3c3af84967
 
     return 0;
 }
