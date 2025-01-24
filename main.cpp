340770509d70609e1265276dd950bc3c3af84967
Kyo young Lim <limkyouyou@gmail.com>
2025-01-24 15:47:21 -0500

    better version

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main() {
 
-    std::cout << "You are Stinky\n";
+    std::cout << "We da best\n";
 
+    // Worst algorithm ever
     for(int i = 1; i > 0; i++){
-        cout << "Get forever looped loser\n"
+        cout << "Get forever looped loser\n";
     }
 
     return 0;
